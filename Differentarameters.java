0daff25f91c241a2d62c569912443ed0d8ee508e
Ratheesh <ratheeshagowda@gmail.com>
2023-08-16 23:36:47 +0530

    single program

---
+++ Differentarameters.java
@@ -0,0 +1,15 @@
+package in.mixedtypeProblems;
+
+public class Differentarameters {
+	
+	public static void main(String[] args) {
+		
+	System.out.println("Ping pong");
+	
+	
+	
+	}
+	
+	public static void main(Integer[] args) {
+}
+}
